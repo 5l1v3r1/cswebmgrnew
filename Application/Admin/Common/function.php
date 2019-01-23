0659e04c983f42ad738f172193fe8fd61742091a
goodman001 <beautyhc001@gmail.com>
2019-01-22 20:00:51 -0800

    update nav

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -16,6 +16,60 @@ function arraySequence($array, $field, $sort = 'SORT_DESC')
     array_multisort($arrSort[$field], constant($sort), $array);
     return $array;
 }
+/*get year total data
+* if year = null  : all data
+*/
+function getAllData($year){
+	$ORDER = M('orders');
+	$revenuesarr = [];
+	$revenues = 0.0;
+	$profit = 0;
+	$salary = 0;
+	/* salary with RMB */
+	$salary = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->where('db_guest_order.g_state = 2 AND db_worker_order.w_state = 3')->sum('db_worker_order.w_payment');
+	/* usd */
+	$revenuesarr  = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->field('db_orders.moneytype,SUM(db_orders.totalprice) as revenues')->where('db_guest_order.g_state = 2 AND db_worker_order.w_state = 3')->group('db_orders.moneytype')->select();
+	$ordernum = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->field('db_orders.orderid,db_orders.createtime,db_guests.wxid as gwxid,db_guests.wxname as gwxname,db_orders.projectname,db_guest_order.g_deadline,db_orders.moneytype,db_orders.totalprice,db_orders.guarantee,db_guest_order.g_state,db_workers.wxid,db_workers.wxname,db_worker_order.w_deadline,db_worker_order.w_payment,db_worker_order.w_state,db_guest_order.remark as gremark,db_orders.description')->where('db_guest_order.g_state = 2 AND db_worker_order.w_state = 3')->count();
+	/* cal total incomplete income  */
+	$ongoingsalary = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->where('db_guest_order.g_state != 2 OR db_worker_order.w_state != 3')->sum('db_worker_order.w_payment');
+	$ongoingrevenuesarr  = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->field('db_orders.moneytype,SUM(db_orders.totalprice) as revenues')->where('db_guest_order.g_state != 2 OR db_worker_order.w_state != 3')->group('db_orders.moneytype')->select();
+	if($salary == ''){
+	$salary = 0;
+	}
+	if($ongoingsalary == ''){
+	$ongoingsalary = 0;
+	}
+	//dump($revenuesarr);
+	$revenuesarrnew = [];
+	foreach($revenuesarr as $k=>$v){
+	  $Model = M('configure_exchange');
+	  $cc['currency'] = $v['moneytype'];
+	  $item = $Model->where($cc)->find();
+	  $v['rating'] = $item['rating'];
+	  $revenuesarr[$k] = $v;
+	  array_push($revenuesarrnew ,$revenuesarr[$k]);
+	  $revenues = $revenues + $v['revenues']*$item['rating'];
+	  //echo $v['revenues']*$item['rating'];
+	}
+	$profit = $revenues - $salary;
+	//dump($revenuesarr);
+	$ongoingrevenues = 0;
+	$ongoingrevenuesarrnew = [];
+	foreach($ongoingrevenuesarr as $k=>$v){
+	  $Model = M('configure_exchange');
+	  $cc['currency'] = $v['moneytype'];
+	  $item = $Model->where($cc)->find();
+	  $v['rating'] = $item['rating'];
+	  $ongoingrevenuesarr[$k] = $v;
+	  array_push($ongoingrevenuesarrnew ,$ongoingrevenuesarr[$k]);
+	  $ongoingrevenues = $ongoingrevenues + $v['revenues']*$item['rating'];
+	  //echo $v['revenues']*$item['rating'];
+	}
+    $ongoingprofit = $ongoingrevenues - $ongoingsalary;
+	return array($revenues, $salary, $profit, $revenuesarrnew,$ordernum,$ongoingrevenues,$ongoingsalary,$ongoingprofit,$ongoingrevenuesarrnew);
+	
+	
+}
 /*get order infomation */
 /*
 
--- Application/Admin/Controller/DashboardController.class.php
+++ Application/Admin/Controller/DashboardController.class.php
@@ -18,85 +18,30 @@ class DashboardController extends CommonController {
       	$this->assign('role',"administrator");
       	$this->assign('description',"This is administrator");
       	$this->assign('nowtime',$nowtime);
-              /* cal total complete income */
-              $ORDER = M('orders');
-              $revenuesarr = [];
-              $revenues = 0.0;
-              $profit = 0;
-              $salary = 0;
-              /* salary with RMB */
-              $salary = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->where('db_guest_order.g_state = 2 AND db_worker_order.w_state = 3')->sum('db_worker_order.w_payment');
-              /* usd */
-              $revenuesarr  = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->field('db_orders.moneytype,SUM(db_orders.totalprice) as revenues')->where('db_guest_order.g_state = 2 AND db_worker_order.w_state = 3')->group('db_orders.moneytype')->select();
-              $ordernum = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->field('db_orders.orderid,db_orders.createtime,db_guests.wxid as gwxid,db_guests.wxname as gwxname,db_orders.projectname,db_guest_order.g_deadline,db_orders.moneytype,db_orders.totalprice,db_orders.guarantee,db_guest_order.g_state,db_workers.wxid,db_workers.wxname,db_worker_order.w_deadline,db_worker_order.w_payment,db_worker_order.w_state,db_guest_order.remark as gremark,db_orders.description')->where('db_guest_order.g_state = 2 AND db_worker_order.w_state = 3')->count();
-              /* cal total incomplete income  */
-              $ongoingsalary = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->where('db_guest_order.g_state != 2 OR db_worker_order.w_state != 3')->sum('db_worker_order.w_payment');
-              $ongoingrevenuesarr  = $ORDER->join('left join db_worker_order on db_worker_order.orderid = db_orders.orderid')->join('left join db_workers on db_worker_order.wxid = db_workers.wxid')->join('left join db_guest_order on db_guest_order.orderid = db_orders.orderid')->join('left join db_guests on db_guest_order.wxid = db_guests.wxid')->field('db_orders.moneytype,SUM(db_orders.totalprice) as revenues')->where('db_guest_order.g_state != 2 OR db_worker_order.w_state != 3')->group('db_orders.moneytype')->select();
-              if($salary == ''){
-              $salary = 0;
-              }
-              if($ongoingsalary == ''){
-              $ongoingsalary = 0;
-              }
-              //dump($revenuesarr);
-              $revenuesarrnew = [];
-              foreach($revenuesarr as $k=>$v){
-                  $Model = M('configure_exchange');
-                  $cc['currency'] = $v['moneytype'];
-                  $item = $Model->where($cc)->find();
-                  $v['rating'] = $item['rating'];
-                  $revenuesarr[$k] = $v;
-                  array_push($revenuesarrnew ,$revenuesarr[$k]);
-                  $revenues = $revenues + $v['revenues']*$item['rating'];
-                  //echo $v['revenues']*$item['rating'];
-              }
-              $profit = $revenues - $salary;
-              //dump($revenuesarr);
-              $ongoingrevenues = 0;
-              $ongoingrevenuesarrnew = [];
-              foreach($ongoingrevenuesarr as $k=>$v){
-                  $Model = M('configure_exchange');
-                  $cc['currency'] = $v['moneytype'];
-                  $item = $Model->where($cc)->find();
-                  $v['rating'] = $item['rating'];
-                  $ongoingrevenuesarr[$k] = $v;
-                  array_push($ongoingrevenuesarrnew ,$ongoingrevenuesarr[$k]);
-                  $ongoingrevenues = $ongoingrevenues + $v['revenues']*$item['rating'];
-                  //echo $v['revenues']*$item['rating'];
-              }
-              $ongoingprofit = $ongoingrevenues - $ongoingsalary;
-              //echo $revenues;
-              //dump($revenues);//complete
-              //dump($salary);//complete
-              //echo "+++";
-              //dump($ongoingrevenues);
-              //dump($ongoingsalary);
+        /* cal total complete income */
 
-              //dump($ongoingrevenues);
-              //dump($ongoingsalary);
-              //$allprofit = $allrevenues - $allsalary;
-              //$allprofit
-              //dump($ongoingprofit);
-              /**/
-              $this->assign('revenues',$revenues);
-              $this->assign('salary',$salary);
-              $this->assign('profit',$profit);
-              $this->assign('revenuesarr',$revenuesarrnew);
-              $this->assign('ordernum',$ordernum );
+        /**/
+        $res = [];
+        $res = getAllData($year);
+        $this->assign('revenues',$res[0]);
+        $this->assign('salary',$res[1]);
+        $this->assign('profit',$res[2]);
+        $this->assign('revenuesarr',$res[3]);
+        $this->assign('ordernum',$res[4]);
 
-              $this->assign('ongoingrevenues',$ongoingrevenues);
-              $this->assign('ongoingsalary',$ongoingsalary);
-              $this->assign('ongoingprofit',$ongoingprofit);
-              $this->assign('ongoingrevenuesarr',$ongoingrevenuesarrnew);
-              $fromdate = date("Y-m-d");
-              $this->assign('today',$fromdate);
-              $this->assign('todaymonth',date("Y-m"));
-              $this->assign('todayyear',date("Y"));
-              /* display */
+        $this->assign('ongoingrevenues',$res[5]);
+        $this->assign('ongoingsalary',$res[6]);
+        $this->assign('ongoingprofit',$res[7]);
+        $this->assign('ongoingrevenuesarr',$res[8]);
+        $fromdate = date("Y-m-d");
+        $this->assign('today',$fromdate);
+        $this->assign('todaymonth',date("Y-m"));
+        $this->assign('todayyear',date("Y"));
+        /* display */
 
-              /*
-                  month show
-              */
+        /*
+          month show
+        */
 
 
 
