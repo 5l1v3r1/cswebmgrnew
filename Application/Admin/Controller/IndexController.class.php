6acac5b0e6ef0519958e7377dbe28427cd13512f
goodman001 <beautyhc001@gmail.com>
2019-06-16 20:15:17 +0800

    update index page

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,8 +2,12 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-  public function index(){
+  public function showlogin(){
     cookie('admin_uid',null);
     $this->display(T('admin/login'));
   }
+  public function index(){
+      $this->display(T('admin/ad'));
+  }
+
 }
